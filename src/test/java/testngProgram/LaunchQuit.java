6633270731510375c97192fa5bd9e52b5ce9cefc
JayashriGore <Jayashrigore5@gmail.com>
2025-04-03 17:11:18 +0530

    My selenium code

--- src/test/java/testngProgram/LaunchQuit.java
+++ src/test/java/testngProgram/LaunchQuit.java
@@ -2,8 +2,9 @@ package testngProgram;
 
 import java.time.Duration;
 
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,7 +15,7 @@ WebDriver driver;
 @BeforeMethod
 public void launch() 
 {
-    driver= new EdgeDriver();
+    driver= new ChromeDriver();
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     driver.get("https://www.amazon.in");
